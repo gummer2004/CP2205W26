8f2a1697083e2a30c7c12d9836ed4e8e6d3d58f1
Ed Byrne <ed.byrne@cna.nl.ca>
2026-01-26 13:29:06 -0330

    Added Edit Save Feature and some UI work

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,9 +1,18 @@
 <x-layout>
-        <x-slot:heading>
+    <x-slot:heading>
         Jobs
     </x-slot>
 
-    <h1>{{$job->title}}</h1>
-    <h2>{{$job->salary}}</h2>
-    <h3>{{$job->employer->name}}</h3>
+    <h1>{{ $job->title }}</h1>
+    <h2>{{ $job->salary }}</h2>
+    <h3>{{ $job->employer->name }}</h3>
+
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+
+                <a href="/jobs/{{$job->id}}/edit"
+                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
+
+        </div>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,48 +7,82 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
 
-    return view('jobs.index',[
-         // 'jobs'=>Job::anpaginate(5),
-       'jobs'=>Job::with('employer')->paginate(10),
+    return view('jobs.index', [
+        // 'jobs'=>Job::anpaginate(5),
+        'jobs' => Job::with('employer')->paginate(10),
     ]);
 });
-Route::get('/jobs/create', function ( ) {
+
+// Create
+Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
-Route::post('/jobs', function (){
 
-//     $job = new Job();
-//     $job->title = request('title');
-//     $job->salary = request('salary');
-//     $job->employer_id = 1;
-//   $job->save();
+// Store
+Route::post('/jobs', function () {
 
-  request()->validate([
-     'title'=> ['required','min:3'] ,
-     'salary'=> ['required'],
-  ]);
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
 
 
-  Job::create([
-    'title' => request('title'),
-    'salary' => request('salary'),
-    'employer_id'=> 1,
-  ]);
-  return redirect('/jobs');
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
 });
 
+
+// show
 Route::get('/jobs/{id}', function ($id) {
 
 
     $job = Job::findOrFail($id);
-    return view('jobs.show',[
-        'job'=> $job,
+    return view('jobs.show', [
+        'job' => $job,
     ]);
 });
 // job create can't go here Route::get('/jobs/create')....
 
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+
+    // get the job
+    $job = Job::findOrFail($id);
+    // show the form
+    return view('jobs.edit', [
+        'job' => $job,
+    ]);
+    // form to hold default data -- happens in the form
+    // send to persist -- happens in the form
+
+});
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    $job = Job::findOrFail($id);
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/'. $job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {});
 
 Route::get('/contact', function () {
     return view('contact');
